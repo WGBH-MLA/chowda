977b3255fac0b7aad997820a3a89f2d2820b4241
Harpo Harbert <ryan_harbert@wgbh.org>
2023-03-22 14:14:52 -0700

    Adds first test!

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from chowda import app
+
+client = TestClient(app)
+
+
+def test_get_admin_home():
+    """GET /admin returns an HTML response"""
+    response = client.get('/')
+    assert response.status_code == 200, 'Admin page did not return sucessfully'
+    assert 'text/html' in response.headers['content-type'], 'Type is not HTML'
