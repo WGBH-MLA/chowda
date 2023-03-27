58b3c4ae0180934359b90988d9d69480ca7ea16e
Harpo Harbert <ryan_harbert@wgbh.org>
2023-03-27 08:50:21 -0700

    Adds PRODUCTION flag

--- chowda/db.py
+++ chowda/db.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
-from .config import ENGINE_URI
+from .config import ENGINE_URI, PRODUCTION
 
-engine = create_engine(ENGINE_URI, connect_args={'check_same_thread': False}, echo=True)
+engine = create_engine(
+    ENGINE_URI, connect_args={'check_same_thread': False}, echo=not PRODUCTION
+)
 
 
 def create_async_engine():
     from sqlmodel.ext.asyncio.session import AsyncEngine
 
     return AsyncEngine(
-        create_engine(ENGINE_URI, connect_args={'check_same_thread': False}, echo=True)
+        create_engine(
+            ENGINE_URI, connect_args={'check_same_thread': False}, echo=not PRODUCTION
+        )
     )
