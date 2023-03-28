68ae44b35bf1baaf488cb929d785e67f23438d37
Harpo <ryan_harbert@wgbh.org>
2023-03-28 12:35:36 -0700

    🏋 Load testing (#11)
    * Adds create user test
    
    * Generalize abstract create_from_factory method
    
    * Adds PRODUCTION flag
    
    * Adds random_model helper function
    
    * Better edit logic: edit random id
    
    * Defines variables for clarity

--- chowda/config.py
+++ chowda/config.py
@@ -1,4 +1,6 @@
 from os import environ
 
+PRODUCTION = bool(environ.get('PRODUCTION', False))
+
 DATABASE_FILE = environ.get('DB_FILE', 'chowda.sqlite')
 ENGINE_URI = environ.get('DB_URL', 'sqlite:///' + DATABASE_FILE)
--- tests/locustfile.py
+++ tests/locustfile.py
@@ -1,15 +1,28 @@
 from locust import HttpUser, task
-from random import shuffle
+from random import choice, randint
+from factories import (
+    UserFactory,
+    MediaFileFactory,
+    CollectionFactory,
+    ClamsAppFactory,
+    PipelineFactory,
+    BatchFactory,
+    ClamsEventFactory,
+)
 
-models = [
-    'user',
-    'media-file',
-    'collection',
-    'clams-app',
-    'pipeline',
-    'batch',
-    'clams-event',
-]
+models = {
+    'user': UserFactory,
+    'media-file': MediaFileFactory,
+    'collection': CollectionFactory,
+    'clams-app': ClamsAppFactory,
+    'pipeline': PipelineFactory,
+    'batch': BatchFactory,
+    # 'clams-event': ClamsEventFactory,
+}
+
+
+def random_model():
+    return choice(list(models.keys()))
 
 
 class HomePageUser(HttpUser):
@@ -18,7 +31,7 @@ class HomePageUser(HttpUser):
         self.client.get('/')
 
 
-class AdminPageUser(HttpUser):
+class AdminUser(HttpUser):
     @task
     def get_admin(self):
         self.client.get('/admin/')
@@ -29,8 +42,41 @@ class AdminPageUser(HttpUser):
 
     @task
     def list_models(self):
-        model_list = models.copy()
-        shuffle(model_list)
-        with self.client.rename_request('admin/[app]/list'):
-            for app in model_list:
-                self.client.get(f'/admin/{app}/list')
+        self.client.get(f'/admin/{random_model()}/list')
+
+    def get_total(self, model: str) -> int:
+        """Use the API to get the total number of records in this model"""
+        response = self.client.get(f'/admin/api/{model}')
+        results = response.json()
+        return results['total']
+
+    @task
+    def get_model_detail(self):
+        model = random_model()
+        total = self.get_total(model)
+        with self.client.rename_request(f'/admin/{model}/detail/[id]'):
+            self.client.get(f'/admin/{random_model()}/detail/{randint(1, total)}')
+
+    @task
+    def edit_model(self):
+        """Update the model through the admin route"""
+        model = random_model()
+        factory = models[model]()
+        widget = factory.build(id=None)
+        total = self.get_total(model)
+        with self.client.rename_request(f'/admin/{model}/edit/[id]'):
+            self.client.post(
+                f'/admin/{model}/edit/{randint(1, total)}',
+                widget.dict(exclude_none=True),
+            )
+
+    def create_from_factory(self, name, factory):
+        """Create a new record using a factory"""
+        factory = factory()
+        widget = factory.build(id=None)
+        self.client.post(f'/admin/{name}/create', widget.dict(exclude_none=True))
+
+    @task
+    def run_factories(self):
+        model = random_model()
+        self.create_from_factory(model, models[model])
