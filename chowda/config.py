a9ddab72db8dbd2ae9647cd8f2ec1af573078f61
Andrew Myers <andrew_myers@wgbh.org>
2023-04-04 14:50:58 -0400

    Adds seed.py for seeding some demo data
    Also:
    * Relates ClamsEvent to Batch, rather than Pipeline. This was a mistake in the data model.
    * Swaps flake8 for ruff 🐶
    * Refactors database engine config
      * Consolidates DB config into db.py
      * Db config is chosen by environment variable ENVIRONMENT, default to 'development'.
      * Test db uses in-memory db, development db uses sqlite. Production db will eventually
        use postres probably, but not defined yet.
    
    Co-authored-by: Harpo Harbert <ryan_harbert@wgbh.org>

--- chowda/config.py
+++
@@ -1,6 +0,0 @@
-from os import environ
-
-PRODUCTION = bool(environ.get('PRODUCTION', False))
-
-DATABASE_FILE = environ.get('DB_FILE', 'chowda.sqlite')
-ENGINE_URI = environ.get('DB_URL', 'sqlite:///' + DATABASE_FILE)
--- chowda/db.py
+++ chowda/db.py
@@ -1,17 +1,31 @@
 from sqlalchemy import create_engine
-from .config import ENGINE_URI, PRODUCTION
+from sqlmodel import SQLModel
+from chowda.models import MediaFile, Collection, Batch, ClamsApp, Pipeline, ClamsEvent
+import os
 
-connect_args = {}
+# All engines for different environments
+engines = {
+    "test": create_engine(
+        'sqlite:///:memory:', connect_args={'check_same_thread': False}, echo=True
+    ),
+    "development": create_engine(
+        'sqlite:///chowda.development.sqlite',
+        connect_args={'check_same_thread': False},
+        echo=True,
+    ),
+    # "production": create_engine(ENGINE_URI, connect_args={'check_same_thread': True}, echo=False)
+}
 
-if not PRODUCTION:
-    connect_args['check_same_thread'] = False
 
-engine = create_engine(ENGINE_URI, connect_args=connect_args, echo=not PRODUCTION)
+# Set the db engine based on which environment we're in, default is 'develpment' unless we're running tests (see above)
+engine = engines.get(os.environ.get('ENVIRONMENT', 'development'))
+
+# Create database from SQLModel schema
+SQLModel.metadata.create_all(engine)
 
 
+# TODO: used anywhere yet?
 def create_async_engine():
     from sqlmodel.ext.asyncio.session import AsyncEngine
 
-    return AsyncEngine(
-        create_engine(ENGINE_URI, connect_args=connect_args, echo=not PRODUCTION)
-    )
+    return AsyncEngine(engine)
