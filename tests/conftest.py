a35ad534df428a94f4f7a4696904a22d260ade5c
Andrew Myers <andrew_myers@wgbh.org>
2023-05-16 01:11:25 -0400

    Adds response cleaning of VCR cassettes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,39 @@
-from os import environ
+from json import dumps, loads
+from json.decoder import JSONDecodeError
+from os import environ, path
+from pytest import fixture
 
 environ['ENVIRONMENT'] = 'test'
+
+
+def clean_response(response: dict):
+    """Replace secrets in response body with dummy values."""
+    try:
+        body = loads(response['body']['string'].decode())
+    except JSONDecodeError:
+        return response
+    if 'access_token' in body:
+        body['access_token'] = 'DUMMY_ACCESS_TOKEN'
+    if 'refresh_token' in body:
+        body['refresh_token'] = 'DUMMY_REFRESH_TOKEN'
+    response['body']['string'] = dumps(body).encode()
+    return response
+
+
+@fixture(scope='module')
+def vcr_config(request):
+    return {
+        # Save cassettes in tests/cassettes/<module_name>/<test_name>.yaml
+        'cassette_library_dir': path.join(
+            path.dirname(path.abspath(__file__)), 'cassettes', request.module.__name__
+        ),
+        # Replace the Authorization request header with "DUMMY" in cassettes
+        'filter_headers': [('authorization', 'Bearer DUMMY')],
+        # Replace secrets in request body
+        'filter_post_data_parameters': [
+            ('client_id', 'FAKE_CLIENT_ID'),
+            ('client_secret', 'FAKE_CLIENT_SECRET'),
+        ],
+        # Replace secrets in response body
+        'before_record_response': clean_response,
+    }
