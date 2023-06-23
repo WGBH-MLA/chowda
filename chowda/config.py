8f4fbc846d76b196328bb83b41d1e7ebb66ddbcd
Andrew Myers <andrew_myers@wgbh.org>
2023-06-23 13:43:03 -0400

    Adds env vars for DB_USER, DB_PASSWORD, DB_HOST, and DB_PORT

--- chowda/config.py
+++ chowda/config.py
@@ -1,7 +1,13 @@
 from os import environ
 
 ENVIRONMENT = environ.get('ENVIRONMENT', 'development')
-DB_URL = environ.get('DB_URL', 'postgresql://postgres:postgres@localhost:5432')
+DB_USER = environ.get('DB_USER', 'postgres')
+DB_PASSWORD = environ.get('DB_PASSWORD', 'postgres')
+DB_HOST = environ.get('DB_HOST', 'localhost')
+DB_PORT = environ.get('DB_PORT', '5432')
+DB_URL = environ.get(
+    'DB_URL', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}'
+)
 DEBUG = bool(environ.get('DEBUG', ENVIRONMENT == 'development'))
 
 TEMPLATES_DIR = environ.get('TEMPLATES_DIR', 'templates')
