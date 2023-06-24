dff95ab41388026a624bba86097c2984ccf3d428
Andrew Myers <andrew_myers@wgbh.org>
2023-06-24 16:10:35 -0400

    Adds pytest args for more informative output on failures Adds @main ref to docs.yml workflow so that vscode's Github Action extension can find it. Ignore unused module import for caveat where models has to be included prior to SQLModel creating db schema in db.py.

--- chowda/db.py
+++ chowda/db.py
@@ -1,7 +1,15 @@
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel
 from chowda.config import DB_URL, DEBUG
-from chowda import models
+
+# NOTE: models must be imported prior to calling SQL.metadata.create_all(engine)
+# Using this method to create the database schems is only temporary until we
+# replace it with a proper migration tool, e.g. Alembic. Disable Ruff linting
+# that flags unused imports: this import *is* used in the sense that once it's
+# imported, it lets SQLModel know about the schema behind the scenes, so that
+# the SQLModel.metadata.create_all(engine) command will actually create the db
+# tables.
+from chowda import models  # noqa: F401
 
 
 engine = create_engine(DB_URL, echo=DEBUG)
