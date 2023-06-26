e861f6dd007b4a2bae1e343ed02753e307846d45
Andrew Myers <andrew_myers@wgbh.org>
2023-06-26 10:51:20 -0400

    Changes db name from 'postgres' to 'chowda'

--- chowda/config.py
+++ chowda/config.py
@@ -4,9 +4,10 @@ ENVIRONMENT = environ.get('ENVIRONMENT', 'development')
 DB_USER = environ.get('DB_USER', 'postgres')
 DB_PASSWORD = environ.get('DB_PASSWORD', 'postgres')
 DB_HOST = environ.get('DB_HOST', 'localhost')
+DB_NAME = 'chowda'
 DB_PORT = environ.get('DB_PORT', '5432')
 DB_URL = environ.get(
-    'DB_URL', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}'
+    'DB_URL', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 )
 DEBUG = bool(environ.get('DEBUG', ENVIRONMENT == 'development'))
 
