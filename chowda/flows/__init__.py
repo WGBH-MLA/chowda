bd23a903396cf466dc434e3f391ea3876b962a80
Harpo Harbert <ryan_harbert@wgbh.org>
2023-06-26 14:13:13 -0700

    Fixes other lint issues

--- chowda/flows/__init__.py
+++ chowda/flows/__init__.py
@@ -1,1 +1,3 @@
 from .ingest import IngestFlow
+
+__all__ = ['IngestFlow']
--- chowda/utils.py
+++ chowda/utils.py
@@ -24,15 +24,15 @@ def chunks_of_size(lst, n):
         yield lst[i : i + n]
 
 
-def chunks_striped(l, n):
+def chunks_striped(lst, n):
     """Yield n number of striped chunks from l."""
     for i in range(0, n):
-        yield l[i::n]
+        yield lst[i::n]
 
 
-def chunks_sequential(l, n):
-    """Yield n number of sequential chunks from l."""
-    d, r = divmod(len(l), n)
+def chunks_sequential(lst, n):
+    """Yield n number of sequential chunks from lst."""
+    d, r = divmod(len(lst), n)
     for i in range(n):
         si = (d + 1) * (i if i < r else r) + d * (0 if i < r else i - r)
-        yield l[si : si + (d + 1 if i < r else d)]
+        yield lst[si : si + (d + 1 if i < r else d)]
