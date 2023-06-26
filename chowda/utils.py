ee54970430b40af3764e867ee868c388707ab40a
Harpo Harbert <ryan_harbert@wgbh.org>
2023-06-26 08:59:19 -0700

    Adds log and chunk utils

--- chowda/utils.py
+++ chowda/utils.py
@@ -16,3 +16,23 @@ def upsert(
             set_=value.dict(),
         )
     )
+
+
+def chunks_of_size(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+def chunks_striped(l, n):
+    """Yield n number of striped chunks from l."""
+    for i in range(0, n):
+        yield l[i::n]
+
+
+def chunks_sequential(l, n):
+    """Yield n number of sequential chunks from l."""
+    d, r = divmod(len(l), n)
+    for i in range(n):
+        si = (d + 1) * (i if i < r else r) + d * (0 if i < r else i - r)
+        yield l[si : si + (d + 1 if i < r else d)]
