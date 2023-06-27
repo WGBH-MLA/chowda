79ce882faddacb83f97200030a27b5cac707983b
Andrew Myers <andrew_myers@wgbh.org>
2023-06-27 14:23:14 -0400

    Configure Alembic to work with Chowda/SQLModel models

--- migrations/env.py
+++ migrations/env.py
@@ -1,5 +1,9 @@
 from logging.config import fileConfig
 
+from chowda import models
+from sqlmodel import SQLModel
+from chowda.config import DB_URL
+
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
@@ -8,6 +12,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", DB_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +69,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
