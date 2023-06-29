7c438828fd15ef86bc3a740356a79b9ad7b5bd4a
Andrew Myers <andrew_myers@wgbh.org>
2023-06-29 14:26:26 -0400

    use dotenv for managing env vars

--- chowda/config.py
+++ chowda/config.py
@@ -1,9 +1,20 @@
 from os import environ
+from dotenv import load_dotenv, find_dotenv
 
 ENVIRONMENT = environ.get('ENVIRONMENT', 'development')
-DEBUG = bool(environ.get('DEBUG', ENVIRONMENT == 'development'))
 
-DB_URL = environ.get('DB_URL', 'sqlite:///chowda.development.sqlite')
+
+dotenv_path = find_dotenv(
+    filename=f'chowda/.env.{ENVIRONMENT}', raise_error_if_not_found=True
+)
+
+
+load_dotenv(dotenv_path, override=True)
+
+DEBUG = bool(environ.get('DEBUG'))
+
+
+DB_URL = environ.get('DB_URL')
 
 TEMPLATES_DIR = environ.get('TEMPLATES_DIR', 'templates')
 STATIC_DIR = environ.get('STATIC_DIR', 'static')
