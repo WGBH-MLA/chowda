b363eba3910d0286bc8c85e272adf74f768abac9
Harpo Harbert <ryan_harbert@wgbh.org>
2023-07-07 08:41:37 -0700

    Fixes DB_NAME env

--- chowda/config.py
+++ chowda/config.py
@@ -13,7 +13,7 @@ load_dotenv(dotenv_path)
 DB_USER = environ.get('DB_USER', 'postgres')
 DB_PASSWORD = environ.get('DB_PASSWORD', 'postgres')
 DB_HOST = environ.get('DB_HOST', 'localhost')
-DB_NAME = 'chowda'
+DB_NAME = environ.get('DB_NAME', 'chowda')
 DB_PORT = environ.get('DB_PORT', '5432')
 DB_URL = environ.get(
     'DB_URL', f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
