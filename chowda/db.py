b7735a7b16109fa69a3300bf6bd81f1c9a8dca3d
Harpo Harbert <ryan_harbert@wgbh.org>
2023-07-07 08:42:49 -0700

    Removes old note

--- chowda/db.py
+++ chowda/db.py
@@ -17,11 +17,4 @@ def init_db():
 
     from chowda import models  # noqa: F401
 
-    # NOTE: models must be imported prior to calling SQL.metadata.create_all(engine)
-    # Using this method to create the database schems is only temporary until we
-    # replace it with a proper migration tool, e.g. Alembic. Disable Ruff linting
-    # that flags unused imports: this import *is* used in the sense that once it's
-    # imported, it lets SQLModel know about the schema behind the scenes, so that
-    # the SQLModel.metadata.create_all(engine) command will actually create the db
-    # tables.
     SQLModel.metadata.create_all(engine)
