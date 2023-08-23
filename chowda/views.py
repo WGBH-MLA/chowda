cc3813919cbbb28d7a5c63833a83ae0e4b874375
Harpo Harbert <ryan_harbert@wgbh.org>
2023-08-23 08:09:42 -0700

    Fixes actions docstrings

--- chowda/views.py
+++ chowda/views.py
@@ -6,15 +6,15 @@ from metaflow import Flow
 from metaflow.exception import MetaflowNotFound
 from metaflow.integrations import ArgoEvent
 from sqlmodel import Session, select
+from starlette.datastructures import FormData
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.templating import Jinja2Templates
-from starlette.datastructures import FormData
 from starlette_admin import CustomView, action
-from starlette_admin.fields import IntegerField, TextAreaField
 from starlette_admin._types import RequestAction
 from starlette_admin.contrib.sqlmodel import ModelView
 from starlette_admin.exceptions import ActionFailed
+from starlette_admin.fields import IntegerField, TextAreaField
 
 from chowda.auth.utils import get_user
 from chowda.db import engine
@@ -189,7 +189,7 @@ class BatchView(BaseModelView):
         submit_btn_class='btn-success',
     )
     async def duplicate_batches(self, request: Request, pks: List[Any]) -> str:
-        """Starts a Batch by sending a message to the Argo Event Bus"""
+        """Create a new batch from the selected batch"""
         try:
             with Session(engine) as db:
                 for batch_id in pks:
@@ -216,7 +216,7 @@ class BatchView(BaseModelView):
         submit_btn_class='btn-success',
     )
     async def combine_batches(self, request: Request, pks: List[Any]) -> str:
-        """Starts a Batch by sending a message to the Argo Event Bus"""
+        """Merge multiple batches into a new batch"""
         try:
             with Session(engine) as db:
                 batches = db.exec(select(Batch).where(Batch.id.in_(pks))).all()
