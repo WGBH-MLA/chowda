b5014dc40a4cfe7441ff4a817fbde39a84038d11
Ryan Harbert (Harpo) <ryan_harbert@wgbh.org>
2023-08-29 12:26:13 -0400

    Adds home page splash screen

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,13 @@ client = TestClient(app)
 def test_get_admin_home():
     """GET / returns a Redirect response"""
     response = client.get('/', allow_redirects=False)
-    assert response.status_code == 307, 'Admin page did not redirect sucessfully'
+    assert response.status_code == 200, 'Home page did not display sucessfully'
+
+
+def test_get_admin_redirect():
+    """GET /admin returns a Redirect response"""
+    response = client.get('/admin', allow_redirects=False)
+    assert response.status_code == 307, 'Home page did not redirect sucessfully'
     assert (
-        '/admin' in response.headers['location']
-    ), 'Home page did not redirect to /admin'
+        '/admin/' in response.headers['location']
+    ), 'Home page did not redirect to /admin/'
