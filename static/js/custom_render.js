6626a5bb8126e64da4b9266237ddaa39f1433096
Andrew Myers <andrew_myers@wgbh.org>
2023-08-30 10:32:39 -0400

    👍 Sony Ci Asset Thumbnails (#135)
    * Displays thumbnails for Sony Ci Asset models
    
    * Adds custom field for displaying thumbnails in Sony Ci Asset detail view.
    * Adds method 'thumbnails_by_type' to Sony Ci Asset model,  which organizes thumbnails
      into a dict keyed by 'type' property.
    * Adds custom render JS function for displaying thumbnails in Sony Ci Asset list view using
      Datatables library.
    
    * Imports SonyCiAssetThumbnail
    
    * Catches null thumnail values
    
    * Fixes fields docstrings
    
    ---------
    
    Co-authored-by: Ryan Harbert (Harpo) <ryan_harbert@wgbh.org>

--- static/js/custom_render.js
+++ static/js/custom_render.js
@@ -2,11 +2,23 @@ Object.assign(render, {
   media_file_guid_links: function render(data, type, full, meta, fieldOptions) {
     // Render a list of media files to a string of links to media files
     return data.map(
-      guid => `<a href="../media-file/detail/${guid}"> ${guid} </a>`
+      (guid) => `<a href="../media-file/detail/${guid}"> ${guid} </a>`
     )
   },
   media_file_count: function render(data, type, full, meta, fieldOptions) {
     // Render a count of media files
     return data.length
+  },
+
+  sony_ci_asset_thumbnail: function render(
+    data,
+    type,
+    full,
+    meta,
+    fieldOptions
+  ) {
+    return data
+      ? `<img src="${data.location}" style="max-height:150px;">`
+      : null
   },
 })
