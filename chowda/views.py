f629b576e654c433eedbdd6c4265cab3e691565f
Harpo Harbert <ryan_harbert@wgbh.org>
2023-09-28 10:23:08 -0700

    Adds Clammer permission to PipelineView

--- chowda/views.py
+++ chowda/views.py
@@ -398,11 +398,8 @@ class ClamsAppView(ClammerModelView):
     fields: ClassVar[list[Any]] = ['name', 'endpoint', 'description', 'pipelines']
 
 
-class PipelineView(AdminModelView):
+class PipelineView(ClammerModelView):
     fields: ClassVar[list[Any]] = ['name', 'description', 'clams_apps']
-
-    def is_accessible(self, request: Request) -> bool:
-        return True
 
 
 class DashboardView(CustomView):
