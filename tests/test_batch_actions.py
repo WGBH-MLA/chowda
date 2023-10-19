b3be2cd658e34803197d5017824077540f7471e9
Andrew Myers <andrew_myers@wgbh.org>
2023-10-19 14:07:48 -0400

    INitials for test of batch action download mmif

---
+++ tests/test_batch_actions.py
@@ -0,0 +1,14 @@
+from httpx import AsyncClient
+import pytest
+
+from chowda.app import app
+
+
+@pytest.mark.asyncio
+async def test_batch_action_download_mmif(async_client: AsyncClient):
+    async with async_client as ac:
+        response = await ac.get(
+            "/admin/api/batch/action", params={"name": "downlaod_mmif", "pks": [2, 3]}
+        )
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Downlaoded MMIF for Test Batch 1, Test Batch 2"
