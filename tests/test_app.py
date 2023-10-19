0be530239f453b244dc8f9116c70e7cc81c57c47
Andrew Myers <andrew_myers@wgbh.org>
2023-10-19 14:06:58 -0400

    uses fixtures instead of local vars for test http client

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,14 @@
 from fastapi.testclient import TestClient
-
 from chowda.app import app
 
-client = TestClient(app)
 
-
-def test_get_admin_home():
+def test_get_admin_home(client: TestClient):
     """GET / returns a Redirect response"""
     response = client.get('/', follow_redirects=False)
     assert response.status_code == 200, 'Home page did not display sucessfully'
 
 
-def test_get_admin_redirect():
+def test_get_admin_redirect(client: TestClient):
     """GET /admin returns a Redirect response"""
     response = client.get('/admin/', follow_redirects=False)
     assert response.status_code == 303, 'Home page did not redirect sucessfully'
