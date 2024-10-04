e78eb6a912a87f85aca3a372de076303dc01f85e
Harpo <ryan_harbert@wgbh.org>
2024-10-04 08:32:30 -0700

    🕹 Buttons (#202)
    * Disables sync button after click, adds animation
    
    * Adds cache for sonyci sync history
    
    * Fixes lint errors and warnings
    
    * Adds dashboard links to runs
    
    * Simplifies row color logic
    
    * Removes tests module
    
    * Adds tests module to include factories in docs  example notebooks
    
    * Adds missing pdm install groups to lockfile
    
    * Uses find for setuptools include: allows missing tests package in production
    
    * Splits icons into own files: successful, in_progress, failed
    
    * Fixes pdm.lock for production
    
    * Actually fixes pdm.lock file
    
    * Cleaner color logic
    
    * Adds finished template
    
    * Fixes other link to finished icon
    
    * Fixes syntax error
    
    * Reduce cache expiration time

--- chowda/routers/sony_ci.py
+++ chowda/routers/sony_ci.py
@@ -1,12 +1,45 @@
 from datetime import datetime
+from typing import Any, Dict
 
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
+from metaflow import Flow
+from metaflow.exception import MetaflowNotFound
 from metaflow.integrations import ArgoEvent
 from pydantic import BaseModel
 
 from chowda.auth.utils import permissions
+from chowda.config import MARIO_URL
 
 sony_ci = APIRouter()
+
+
+# The startup / shutdown lifecycle events are deprecated, but the lifespan event handler
+# does not currently work with APIRouter, even though it accepts the lifespan argument.
+# https://github.com/tiangolo/fastapi/discussions/9664
+@sony_ci.on_event('startup')
+async def lifespan():
+    FastAPICache.init(InMemoryBackend())
+    await sync_history()
+
+
+@cache(namespace='sonyci', expire=30)
+async def sync_history(n: int = 3) -> Dict[str, Any]:
+    try:
+        return [
+            {
+                'created_at': sync_run.created_at,
+                'finished': sync_run.finished,
+                'finished_at': sync_run.finished_at,
+                'successful': sync_run.successful,
+                'link': MARIO_URL + sync_run.pathspec,
+            }
+            for sync_run in list(Flow('IngestFlow'))[:n]
+        ]
+    except MetaflowNotFound:
+        return []
 
 
 class SyncResponse(BaseModel):
@@ -19,6 +52,7 @@ class SyncResponse(BaseModel):
 async def sony_ci_sync() -> SyncResponse:
     try:
         ArgoEvent('sync').publish(ignore_errors=False)
+        FastAPICache.clear(namespace='sonyci')
         return SyncResponse(started_at=datetime.utcnow())
     except Exception as error:
         raise HTTPException(status_code=500, detail={'error': str(error)}) from error
