e3b3523cd82baf1f619ea41b0f5f37ed71639699
Harpo Harbert <ryan_harbert@wgbh.org>
2026-06-04 14:43:15 -0700

    Try: use custom metaflow fork to test aws secrets access

--- chowda/flows/ingest.py
+++ chowda/flows/ingest.py
@@ -1,4 +1,4 @@
-from metaflow import FlowSpec, step, trigger, secrets
+from metaflow import FlowSpec, step, trigger, secrets, kubernetes
 
 from chowda.log import log
 from chowda.models import AssetType
