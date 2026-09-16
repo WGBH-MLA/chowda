93e68859ea2c222928127b14d393c7d38904e432
Harpo Harbert <ryan_harbert@wgbh.org>
2026-09-16 10:45:03 -0700

    Bumps version

--- chowda/api.py
+++ chowda/api.py
@@ -6,5 +6,3 @@ api = APIRouter()
 
 api.include_router(events, prefix='/event')
 api.include_router(sony_ci, prefix='/sonyci')
-# Legacy prefix, kept so existing SonyCi API clients keep working.
-api.include_router(sony_ci, prefix='/sony_ci', include_in_schema=False)
--- chowda/app.py
+++ chowda/app.py
@@ -94,7 +94,7 @@ admin.add_view(SonyCiAssetView(SonyCiAsset, icon='fa fa-file-video'))
 admin.add_view(
     SonyCiTrashbinView(SonyCiTrashbin, icon='fa fa-trash', label='SonyCi Trashbin')
 )
-admin.add_view(SonyCiEventView(SonyCiEvent, icon='fa fa-bell'))
+admin.add_view(SonyCiEventView(SonyCiEvent, icon='fa fa-c'))
 admin.add_view(CollectionView(Collection, icon='fa fa-folder'))
 admin.add_view(BatchView(Batch, icon='fa fa-folder'))
 admin.add_view(ClamsAppView(ClamsApp, icon='fa fa-box'))
